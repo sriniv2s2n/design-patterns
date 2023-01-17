026c9b23da9fecccbc4896b22b896ce111464dc0
sriniv2s2n <42947267+sriniv2s2n@users.noreply.github.com>
2023-01-17 12:57:05 +0530

    Add module pattern

---
+++ module-pattern-1.js
@@ -0,0 +1,7 @@
+import {
+  add as addValues,
+  sub as subValues,
+  mul as mulValues,
+} from './module-pattern-2';
+
+console.log(addValues(10, 5), subValues(10, 5), mulValues(10, 5));
---
+++ module-pattern-2.js
@@ -0,0 +1,3 @@
+export const add = (num1, num2) => num1 + num2;
+export const sub = (num1, num2) => num1 - num2;
+export const mul = (num1, num2) => num1 * num2;
